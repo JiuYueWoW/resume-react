91b2b3f38f7d1d9fffbe7745a713cce2e51d36d6
LittleSource <444168950@qq.com>
2022-06-13 21:15:36 +0800

    first commit

---
+++ src/data/skill.ts
@@ -0,0 +1,42 @@
+const SkillData = [
+    {
+      title: 'Web',
+      item: [
+        "熟悉 Vue、React等前端框架的使用",
+        "掌握 UNI-APP、Taro 开发夸端应用",
+        "掌握微信小程序及微信支付的开发与部署",
+      ]
+    },{
+      title: 'PHP',
+      item: [
+        "掌握 PHP 的基本知识",
+        "掌握 ThinkPHP等框架的使用",
+        "掌握 LNMP栈开发、熟悉OOP开发思想",
+        "了解 SwooleEasySwoole等协程框架"
+      ]
+    },{
+      title: 'Golang',
+      item: [
+        "掌握 go语言 的基本知识",
+        "熟悉 Gin、Beego等Web框架的使用",
+        "熟悉 goroutine、channel使用",
+        "了解 微服务、gRPC、K8S",
+        "了解 kratos微服务框架"
+      ]
+    },  {
+      title: 'Database',
+      item: [
+        "熟悉 MySQL 等关系型数据库",
+        "熟悉 MongoDB、Redis非关系型数据库",
+      ]
+    },  {
+      title: 'Others',
+      item: [
+        "熟悉常用的Linux命令及云服务器常用操作",
+        "熟悉LNPM配置与使用",
+        "熟悉Docker及镜像的基本使用",
+        "熟练使用 Git 进行代码管理、 Markdown进行文档编写",
+      ]
+    }
+]
+export default SkillData
