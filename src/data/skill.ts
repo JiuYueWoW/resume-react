0c2b596a785e059372e9ebf7b83036b5d84e4abc
LittleSource <444168950@qq.com>
2022-07-16 16:09:02 +0800

    complete

--- src/data/skill.ts
+++ src/data/skill.ts
@@ -1,41 +1,29 @@
 const SkillData = [
-    {
-      title: 'Web',
-      item: [
-        "熟悉 Vue、React前端框架",
-        "熟悉Vite插件开发",
-        "掌握 UNI-APP、Taro 开发夸端应用",
-        "掌握微信小程序开发",
-      ]
-    },{
-      title: 'PHP',
-      item: [
-        "掌握 PHP 的基本知识",
-        "掌握 ThinkPHP等框架的使用",
-        "掌握 LNMP栈开发、熟悉OOP开发思想",
-        "了解 SwooleEasySwoole等协程框架"
-      ]
-    },{
-      title: 'Golang',
-      item: [
-        "掌握 go语言 的基本知识",
-        "熟悉 Gin 等Web框架的使用",
-        "熟悉 goroutine、channel使用",
-        "了解 kratos微服务框架"
-      ]
-    },  {
-      title: 'Database',
-      item: [
-        "熟悉 MySQL 等关系型数据库",
-        "熟悉 MongoDB、Redis非关系型数据库",
-      ]
-    },  {
-      title: 'Others',
-      item: [
-        "熟悉常用的Linux命令及云服务器常用操作",
-        "熟悉LNPM配置与使用",
-        "熟悉Docker及镜像的基本使用",
-      ]
-    }
-]
-export default SkillData
+  {
+    title: '前端技能',
+    item: [
+      '熟悉HTML、CSS、 JavaScript/TypeScript的基础知识， 熟悉ES6常用的语法和API。',
+      '熟悉Vue.js，能够结合Vuex、Vue Router、UI库等扩展技能，能够开发自定义组件。',
+      '熟悉UNI-APP和微信小程序开发，熟悉微信公众号及小程序相关API。',
+      '了解React, 能够结合React Router、 Redux、 UI库等扩展技能开发页面。',
+      '了解Vite、Webpack,并使用其搭建开发环境。',
+    ],
+  },
+  {
+    title: '后端技能',
+    item: [
+      '熟悉 OOP开发思想、LNMP栈',
+      '熟悉 PHP, 熟悉ThinkPHP、Swoole等框架开发',
+      '熟悉 Golang,熟悉 Gin等Web框架的使用,了解 kratos微服务框架',
+    ],
+  },
+  {
+    title: '其他技能',
+    item: [
+      '熟悉Docker及镜像的基本使用',
+      '熟悉常用的Linux命令及云服务器常用操作',
+      '熟悉 MySQL、MongoDB、Redis等关系型数据库',
+    ],
+  },
+];
+export default SkillData;
